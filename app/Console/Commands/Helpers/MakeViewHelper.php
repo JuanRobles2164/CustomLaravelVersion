857600d608820ffe8a28b49e937a08adac24fcb7
JuanRobles2164 <juanestebanrobles3103@gmail.com>
2020-07-12 01:06:41 -0500

    Create MakeViewHelper.php
    Crea la vista en base a, la plantilla maestra que se le asigna
    La cantidad de secciones que quiere que contenga la vista

---
+++ app/Console/Commands/Helpers/MakeViewHelper.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Console\Commands\Helpers;
+class MakeViewHelper{
+
+    /**
+     * @param string Cadena que representa la plantilla maestra a utilizar. Ej: Master_Layout
+     * @param array Array que representa las secciones de las que comprenderá la vista. Ej: content,custom_scripts
+     * @return string Cadena que representa el contenido del archivo a generar
+     */
+    public static function BasicViewContent($parent_layout='Layout', $section = null){
+        if($section == null){
+            $section = ['content'];
+        }
+        $content = "@extends('$parent_layout')\n";
+        foreach($section as $name){
+            $content = $content . "
+@section('$name')
+
+@endsection
+
+            ";
+        }
+        return $content;
+    }
+}
